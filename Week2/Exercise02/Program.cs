612fe6c11f76a9a681c48bb9284646e4ff251ebd
Litepac <rasmusmp94@gmail.com>
2025-09-08 14:41:34 +0200

    Test Test

--- Week2/Exercise02/Program.cs
+++ Week2/Exercise02/Program.cs
@@ -8,7 +8,10 @@ Console.WriteLine("Hello, World!");
 Person person = new()
 {
     Name = "Peter"
+    , Age = 30
 };
 
 
 Console.WriteLine($"The name of the person is {person.Name}");
+
+Console.WriteLine($"The age of the person is {person.Age}");
